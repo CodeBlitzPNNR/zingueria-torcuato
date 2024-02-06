63a7668cbcfd31d2aa69e87e6c02f80bfa5e0166
Ramiro Savoy <rawii.k@gmail.com>
2024-02-06 18:35:29 -0300

    Landing solamente

--- dashboard/src/app/dashboard/cliente/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home({ cliente }) {
-    return (
-      <main className="min-h-[100dvh] flex flex-col justify-center items-center">
-        Cliente
-      </main>
-    );
-  }
